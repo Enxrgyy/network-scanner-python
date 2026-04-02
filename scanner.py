b93ca37dd6cc25bf5dc3a01961a006850f4e188f
Enxrgyy <matias.peralta09@inacapmail.cl>
2026-04-02 14:10:08 -0300

    agregar host

--- scanner.py
+++ scanner.py
@@ -5,3 +5,8 @@ import argparse
 parser = argparse.ArgumentParser(description="Escáner de Red - INACAP")
 parser.add_argument("-t", "--target", help="Red base (ej: 192.168.1.)")
 args = parser.parse_args()
+
+def ping(host):
+    comando = ["ping", "-n", "1", "-w", "500", host]
+    resultado = subprocess.run(comando, capture_output=True, text=True)
+    return resultado.returncode == 0
